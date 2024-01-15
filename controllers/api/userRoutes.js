4210c543c099240f09c0957946a8dcb3edd9dc68
quinncombs <qcombs31@gmail.com>
2024-01-14 20:56:51 -0500

    handlebars added and js login/out

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
+// api/users
 router.get('/', req, res => {
     User.findAll({
         attributes: { exclude: ['password' ]}
@@ -14,3 +14,33 @@ router.get('/', req, res => {
     });
 });
 
+// login
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
+
+module.exports = router;
